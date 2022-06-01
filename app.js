c06d19ba54b01f54b56a61d5be3ce378cda06ed9
GwynbleiddSeven <101643987+GwynbleiddSeven@users.noreply.github.com>
2022-06-01 00:27:45 -0500

    Correciones, adiciones y finalizado

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ iconoMenu.addEventListener('click', (e) => {
     // Alternamos su atributo 'src' para el ícono del menú
     const rutaActual = e.target.getAttribute('src');
 
-    if(rutaActual == 'img/open-menu.png'){
+    if(rutaActual == 'img/open-menu2.png'){
         e.target.setAttribute('src','img/open-menu2.png');
     }else{
-        e.target.setAttribute('src','img/open-menu.png');
+        e.target.setAttribute('src','img/open-menu2.png');
     }
 });
 function close_tab() {
